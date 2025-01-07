32e2a5cbc9a2a664c91afea462e62735e69a8914
abhijit_sarker <abhijit.sarker@sjinnovation>
2025-01-07 12:41:41 +0600

    add markdown support and new schema types for playlists and startups

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -23,16 +23,6 @@ export default async function Home({ searchParams}: { searchParams: Promise<{ qu
         <p className="text-30-semibold">
           {query ? `Search results for "${query}"` : "All Startups"}
         </p>
-
-        {/* <ul className="mt-7 card_grid">
-          {posts?.length > 0 ? (
-            posts.map((post: StartupTypeCard) => (
-              <StartupCard key={post?._id} post={post} />
-            ))
-          ) : (
-            <p className="no-results">No startups found</p>
-          )}
-        </ul> */}
       </section>
     </ div>
   );
