a5eaeb1ddc6140c8d00be2ad4d453ccd2c81d7a7
abhijit_sarker <abhijit.sarker@sjinnovation>
2025-01-07 12:02:27 +0600

    added sanity

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -18,6 +18,22 @@ export default async function Home({ searchParams}: { searchParams: Promise<{ qu
 
         <SearchForm query={query} />
       </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        {/* <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul> */}
+      </section>
     </ div>
   );
 }
